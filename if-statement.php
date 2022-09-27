311f49089fe65dcd9dc263d8399c23d24b258f82
Michael <mtanaya09@gmail.com>
2022-09-27 23:53:02 +0800

    IF STATEMENT

--- if-statement.php
+++ if-statement.php
@@ -1,2 +1,21 @@
-<?php
-    echo "yoo";
+<html>
+
+<body>
+
+    <?php
+    $isMale = false;
+    $isTall = true;
+    if ($isMale && $isTall) {
+        echo "Your are a TALL MALE";
+    } elseif ($isMale && !$isTall) {
+        echo "You are a SHORT MALE";
+    } elseif (!$isMale && $isTall) {
+        echo "Your not MALE but you are TALL";
+    } else {
+        echo "Your are not male and not tall";
+    }
+    ?>
+
+</body>
+
+</html>
