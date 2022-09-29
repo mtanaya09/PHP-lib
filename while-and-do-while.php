5824e47115760cf6a51f4a252052b43b529185ae
Michael <mtanaya09@gmail.com>
2022-09-30 00:41:17 +0800

    DO | DO WHILE LOOPS

---
+++ while-and-do-while.php
@@ -0,0 +1,17 @@
+<?php
+
+// WHILE LOOPS
+
+// $index = 1;
+// while($index <= 5){
+//     echo "$index <br>";
+//     $index++;
+// }
+
+// DO WHILE LOOPS
+
+$index = 6;
+do {
+    echo "$index <br>";
+    $index++;
+} while ($index <= 5);
