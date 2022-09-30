ee070797a6c544f3695b5f07d1afe6f7db46c0fc
Michael <mtanaya09@gmail.com>
2022-10-01 01:15:55 +0800

    INCLUDE HTML | PHP

---
+++ article-header.php
@@ -0,0 +1,3 @@
+<h1><?php echo $title ?></h1>
+<h3><?php echo $author ?></h3>
+Word count: <?php echo $wordCount ?>
---
+++ sample-page2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAGE 2</title>
+</head>
+
+<body>
+
+
+    <?php
+    $title = "SAPLE PAGE 2";
+    $author = "kelsyyy";
+    $wordCount = 300;
+    include "article-header.php";
+
+    ?>
+
+
+</body>
+
+</html>
